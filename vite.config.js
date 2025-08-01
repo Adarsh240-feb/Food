0c37a01948cb944f00446915a49ac43f901f8898
Adarsh Kesharwani <kesharwaniadarsh24@gmail.com>
2025-08-02 01:23:49 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add this line:
+  base: '/Food/', 
   plugins: [react()],
-  base: '/FoodDel/', // <-- Add this line
 })
+
