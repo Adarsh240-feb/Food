7be1ae8fc509a2c94f8f649f512bc2c6298b1cc0
Adarsh Kesharwani <kesharwaniadarsh24@gmail.com>
2025-08-02 01:35:36 +0530

    fix: wrap entire app in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom' // <-- 1. IMPORT THIS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. ADD THE BROWSER ROUTER WRAPPER AND BASENAME PROP  */}
+    <BrowserRouter basename="/Food">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
